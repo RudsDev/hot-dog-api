cfa9c0cd9d60cc64acfc40c20d0b0bb41df07378
Rafael Ulisses <ruds.devel@gmail.com>
2024-02-10 15:10:33 -0300

    First

---
+++ src/main/java/dev/ruds/hotdog/ApiHotDogs.java
@@ -0,0 +1,13 @@
+package dev.ruds.hotdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHotDogs {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHotDogs.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ruds/hotdog/HotDogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruds.hotdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
