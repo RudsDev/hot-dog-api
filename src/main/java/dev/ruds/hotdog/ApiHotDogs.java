2f0c1767246693a5c4b8cbb1fe7407a1162b48ef
Rafael Ulisses <ruds.devel@gmail.com>
2024-02-11 11:02:07 -0300

    Alteração estratéga de inicialização

--- src/main/java/dev/ruds/hotdog/ApiHotDogs.java
+++ src/main/java/dev/ruds/hotdog/ApiHotDogs.java
@@ -2,12 +2,21 @@ package dev.ruds.hotdog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiHotDogs {
+@EntityScan("dev.ruds.hotdog.domain.models")
+public class ApiHotDogs extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiHotDogs.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiHotDogs.class);
 	}
 
 }
