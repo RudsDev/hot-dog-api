96dc6d4927c400ebb76cef87d835b2f5525e307c
Rafael Ulisses <ruds.devel@gmail.com>
2024-03-15 15:28:45 -0300

    Revert "Alteração na PromocaoMapper para evitar que o LAZY LOADING seja bypassado"
    This reverts commit bd3c7cd2165524230b546c4d945f14584912385f.

--- src/main/java/dev/ruds/hotdog/utils/mappers/PromocaoMapper.java
+++ src/main/java/dev/ruds/hotdog/utils/mappers/PromocaoMapper.java
@@ -1,15 +1,20 @@
 package dev.ruds.hotdog.utils.mappers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import dev.ruds.hotdog.domain.models.Promocao;
 import dev.ruds.hotdog.domain.records.PromocaoOutputRecord;
 
 @Component
 public class PromocaoMapper {
+    
+    @Autowired
+    private ObjectMapper mapper;
 
     public PromocaoOutputRecord toRecord(Promocao promocao) {
-        return new PromocaoOutputRecord(promocao.getId(), promocao.getNome(), 
-        promocao.getBaseCalculo(), promocao.getTipoCalculo());
+        return mapper.convertValue(promocao, PromocaoOutputRecord.class);
     }
 }
